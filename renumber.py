02dd9aca9b3157002efb07e563cb00115b719d3e
Pete Brooks <pete.brooks@tablexi.com>
2019-02-04 02:29:55 -0600

    Add padding to renumber

--- renumber.py
+++ renumber.py
@@ -23,14 +23,25 @@ class Renumber(lxu.command.BasicCommand):
             self.selection = []
 
         self.dyna_Add('Base Name', lx.symbol.sTYPE_STRING)
-        self.dyna_Add('Start Name', lx.symbol.sTYPE_STRING)
-        self.dyna_Add('End Name', lx.symbol.sTYPE_STRING)
         self.basic_SetFlags(0, lx.symbol.fCMDARG_QUERY)
+
+        self.dyna_Add('Padding', lx.symbol.sTYPE_INTEGER)
+        self.basic_SetFlags(1, lx.symbol.fCMDARG_QUERY)
+        self.basic_SetFlags(1, lx.symbol.fCMDARG_OPTIONAL)
+
+        self.dyna_Add('Start Name', lx.symbol.sTYPE_STRING)
+        self.basic_SetFlags(2, lx.symbol.fCMDARG_QUERY)
+        self.basic_SetFlags(2, lx.symbol.fCMDARG_OPTIONAL)
+
+        self.dyna_Add('End Name', lx.symbol.sTYPE_STRING)
+        self.basic_SetFlags(3, lx.symbol.fCMDARG_QUERY)
+        self.basic_SetFlags(3, lx.symbol.fCMDARG_OPTIONAL)
 
     def basic_Execute(self, msg, flags):
         baseName = self.cleanString(self.dyna_String(0))
-        startName = self.cleanString(self.dyna_String(1))
-        endName = self.cleanString(self.dyna_String(2))
+        padding = self.dyna_Int(1, 2)
+        startName = self.cleanString(self.dyna_String(2))
+        endName = self.cleanString(self.dyna_String(3))
 
         if baseName:
             for i, item in enumerate(self.selection, start=1):
@@ -39,26 +50,28 @@ class Renumber(lxu.command.BasicCommand):
                 elif endName and i == len(self.selection):
                     item.SetName(baseName + endName)
                 elif startName:
-                    item.SetName(baseName + str(i - 1))
+                    item.SetName(baseName + str(i - 1).zfill(padding))
                 else:
-                    item.SetName(baseName + str(i))
+                    item.SetName(baseName + str(i).zfill(padding))
         else:
             lx.out('Base name invalid')
 
     def cmd_Query(self, index, vaQuery):
-        """
-        Put the name of the first selected item in the dialog's name field.
-        """
+        '''
+        Add field defaults.
+        '''
         ValueArray = lx.object.ValueArray()
         ValueArray.set(vaQuery)
-        ValueArray.AddString(self.selection[0].UniqueName())
+        ValueArray.AddString(self.selection[0].UniqueName()) # Base Name
         return lx.result.OK
 
     def cmd_Flags(self):
         return lx.symbol.fCMD_MODEL | lx.symbol.fCMD_UNDO
 
     def cmd_Interact(self):
-        # Prevent bogus stack trace errors.
+        '''
+        Prevent bogus stack trace errors.
+        '''
         pass
 
     def cleanString(self, val=None):
@@ -67,7 +80,7 @@ class Renumber(lxu.command.BasicCommand):
 
         illegalChars = ['^', '<', '>', '/', '\\', '{', '}', '[', ']',
                         '~', '`', '$', '.', '?', '%', '&', '@', '*',
-                        '(', ')', '!', '+', '#', '\'', '\"']
+                        '!', '+', '#', '\'', '\"']
 
         for char in illegalChars:
             val = val.replace(char, '')
